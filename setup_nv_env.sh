0b77ac7bbf17c34450341bb0eb92f1191cd2bfb0
allen <allen89koujp@gmail.com>
2025-11-20 15:21:23 +0800

    update shell script

--- setup_nv_env.sh
+++ setup_nv_env.sh
@@ -84,8 +84,8 @@ install_driver() {
 # ====================================================
 install_cuda() {
     log "Installing CUDA 13 keyring"
-    UBUNTU_VER=$(lsb_release -rs | sed 's/\\.//g')
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu$UBUNTU_VER/x86_64/cuda-keyring_1.1-1_all.deb
+    UBUNTU_VER=$(lsb_release -rs | tr -d '.\r')
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VER}/x86_64/cuda-keyring_1.1-1_all.deb
     sudo dpkg -i cuda-keyring_1.1-1_all.deb
     sudo apt update
 
